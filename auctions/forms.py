7a95721f89254deb70fd2ba48bda6181d519e9bb
Gyanashree Giri <giri.gyanashree347@gmail.com>
2022-03-15 12:40:54 +0545

    Add styling to create listing form

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,8 +1,13 @@
 
+from logging import PlaceHolder
 from .models import Auction_listing
 from django import forms
 
 class AuctionForm(forms.ModelForm):
+     long_description = forms.CharField(widget=forms.Textarea)
+     short_description = forms.CharField(widget=forms.Textarea)
+     
      class Meta:
         model = Auction_listing
-        exclude = ['owner']
+        
+        exclude = ['owner', 'bidOpen', 'winner']
--- auctions/models.py
+++ auctions/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+
 class User(AbstractUser):
     pass
 
