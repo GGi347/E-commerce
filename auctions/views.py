1349e5f98f3af5a86989b832c91dd59048060fdb
Gyanashree Giri <giri.gyanashree347@gmail.com>
2022-03-24 16:40:01 +0530

    All category to show all listings. Fix styling."

--- auctions/views.py
+++ auctions/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -8,7 +8,7 @@ from auctions.forms import AuctionForm
 from .models import Auction_listing, Comments, User, Watchlist, Bids, Category
 from django.contrib import messages
 
-
+# the home page that shows active listings 
 def index(request):
     all_items = Auction_listing.objects.all()
     items = []
@@ -76,6 +76,7 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
+# users create new listings. New listings are always set to active by default.
 @login_required
 def create_listing(request):
     if request.method == "POST":
@@ -87,12 +88,14 @@ def create_listing(request):
         owner = request.user
         item = Auction_listing.objects.create(name=name, price=price, short_description=short_description, long_description=long_description, imageUrl=imageUrl, owner=owner)
         item.save()
+        # after adding new item, redirected to home index page
+        return HttpResponseRedirect(reverse("index"))
     return render(request, "auctions/create_listing.html", {
         "form": AuctionForm()
     })
 
     
-
+# winner of the bid and the max_bid 
 def get_max_bid(item):
     bids = list(Bids.objects.filter(item=item.id))       
     max_bid = item.price
@@ -105,16 +108,15 @@ def get_max_bid(item):
 
     return (max_bid, winner)
 
+# shows individual item in detail. also has options for registered users to bid and add to wishlist.
 def item(request, item_id):
     item = Auction_listing.objects.get(pk=item_id)
-    # watch_item = Watchlist.objects.all()
-    # print(watch_item)
     bids= Bids.objects.filter(item=item_id).count() 
     comments = list(Comments.objects.filter(item=item_id))
     is_watch_item = list(Watchlist.objects.filter(user=request.user.id, item=item_id))
     is_owner = True if item.owner == request.user else False
+    # get_max_bid function to get maximum bidding made so far
     max_bid = get_max_bid(item)[0]
-    print(is_owner, item.owner, request.user)
     return render(request, "auctions/item.html", {
         "item": item,
         "is_watch_item": is_watch_item,
@@ -124,6 +126,7 @@ def item(request, item_id):
         "max_bid": max_bid,
     })
 
+# wishlist of active items or remove items from wishlist
 @login_required
 def watchlist(request, item_id):
     user = request.user
@@ -135,9 +138,7 @@ def watchlist(request, item_id):
     else:
         messages.add_message(request, messages.INFO, f'Item added to the wishlist.')
         item_to_add = Watchlist.objects.create(item= item, user=user)
-        item_to_add.save()
-
-    
+        item_to_add.save()   
     return HttpResponseRedirect(reverse("item", args=(item_id,) ))
 
 
@@ -155,6 +156,7 @@ def show_watchlist(request):
         "header": "WatchList"
     })
 
+# items that are closed for bidding.
 def closed_listing(request):
     all_items = Auction_listing.objects.all()
     items = []
@@ -167,12 +169,14 @@ def closed_listing(request):
     })
 
 
-
+@login_required
 def place_bid(request, item_id):
     if request.method == "POST":
         item = Auction_listing.objects.get(pk=item_id)
+        # get the current maximum bid
         max_bid = get_max_bid(item)[0]
         curr_bid = float(request.POST["bid-amount"])
+        # allow to place bid only if the bid is more than current maximum bid
         if curr_bid < max_bid:
             messages.add_message(request, messages.INFO, f'Please place a bid higher than Rs.{max_bid}')
             return HttpResponseRedirect(reverse("item", args=(item_id, )))
@@ -182,18 +186,19 @@ def place_bid(request, item_id):
     return HttpResponseRedirect(reverse("item", args=(item_id, )))
 
 
-
+@login_required
 def close_bid(request, item_id):
     item = Auction_listing.objects.get(pk=item_id)
     item.bidOpen = False
+    # get winner of bid
     winner = get_max_bid(item)[1]
     item.winner = winner
     item.save()
-    print("winner ", winner)
-    print("user", request.user)
     messages.add_message(request, messages.INFO, "The bid has been closed.")
     return HttpResponseRedirect(reverse("item", args=(item_id, )))
 
+# adds comments by users for items
+@login_required
 def add_comment(request, item_id):
     if request.method == "POST":
         item = Auction_listing.objects.get(pk=item_id)
@@ -202,14 +207,19 @@ def add_comment(request, item_id):
         new_comment.save()
     return HttpResponseRedirect(reverse("item", args=(item_id, )))
 
-
+# lists the available categories in index page. 
 def category_listing(request, category_id):
     category = Category.objects.get(pk=category_id)
     categories = Category.objects.all()
-    all_items = category.item.all()
+    # show all listings if category is all
+    if (category.name) == "All":
+        all_items = Auction_listing.objects.all()
+    else:
+        all_items = category.item.all()
     items = []
     for i in all_items:
         item = Auction_listing.objects.get(pk=i.id)
+        # show only active listings
         if item.bidOpen:
             items.append(item)
     return render(request, "auctions/index.html", {
